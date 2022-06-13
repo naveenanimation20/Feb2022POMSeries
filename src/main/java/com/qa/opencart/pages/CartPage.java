4d1d0a6fa0ec2b7f5d2adc6413a1afc017f21947
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-06-13 18:35:46 +0530

    updated cart page

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 public class CartPage {
 	
 	private By cart = By.id("page-cart");
+	private By item = By.id("page-item");
+
 	
 	public void printCart() {
 		System.out.println("cart items");
